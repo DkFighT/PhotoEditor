0ff25ddad70ba2746b8e8187aef34e03b42e1fff
DkFighT <116903571+DkFighT@users.noreply.github.com>
2023-05-09 17:27:29 +0300

    Add files via upload

--- imageeditor/main.py
+++ imageeditor/main.py
@@ -1,7 +1,7 @@
 import customtkinter as ct, ctypes, os, PIL
 from tkinter.filedialog import askopenfilename
 from tkinter import Tk, Canvas, Frame, BOTH, NW
-from PIL import Image, ImageTk, ImageFilter
+from PIL import Image, ImageTk, ImageFilter, ImageEnhance
 
 user32 = ctypes.windll.user32
 screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
@@ -22,7 +22,7 @@ class App(ct.CTk):
         # control panel
         self.controls = ct.CTkFrame(self, width=200, corner_radius=10)
         self.controls.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
-        self.controls.grid_rowconfigure(10, weight=1)
+        self.controls.grid_rowconfigure(12, weight=1)
         # control buttons
         self.zoom = ct.CTkLabel(self.controls, text='Zoom', text_color='#ffffff', bg_color='transparent')
         self.zoom.grid(row=0, column=0, sticky="ew", pady=10)
@@ -34,24 +34,29 @@ class App(ct.CTk):
         self.rotate_btn = ct.CTkSlider(self.controls, from_=-180, to=180, command=self.rotate_func)
         self.rotate_btn.grid(row=3, column=0, sticky="ew", padx=10)
 
-        self.red_lbl = ct.CTkLabel(self.controls, text='Red', text_color='#ffffff', bg_color='transparent')
+        self.red_lbl = ct.CTkLabel(self.controls, text='Saturation', text_color='#ffffff', bg_color='transparent')
         self.red_lbl.grid(row=4, column=0, sticky="ew", pady=10)
-        self.red_btn = ct.CTkSlider(self.controls, from_=0, to=255, command=self.red_func)
+        self.red_btn = ct.CTkSlider(self.controls, from_=0, to=200, command=self.enchance_func)
         self.red_btn.grid(row=5, column=0, sticky="ew", padx=10)
 
-        self.green_lbl = ct.CTkLabel(self.controls, text='Green', text_color='#ffffff', bg_color='transparent')
+        self.green_lbl = ct.CTkLabel(self.controls, text='Contrast', text_color='#ffffff', bg_color='transparent')
         self.green_lbl.grid(row=6, column=0, sticky="ew", pady=10)
-        self.green_btn = ct.CTkSlider(self.controls, from_=0, to=255, command=self.green_func)
+        self.green_btn = ct.CTkSlider(self.controls, from_=0, to=200, command=self.contrast_func)
         self.green_btn.grid(row=7, column=0, sticky="ew", padx=10)
 
-        self.blue_lbl = ct.CTkLabel(self.controls, text='Blue', text_color='#ffffff', bg_color='transparent')
+        self.blue_lbl = ct.CTkLabel(self.controls, text='Brightness', text_color='#ffffff', bg_color='transparent')
         self.blue_lbl.grid(row=8, column=0, sticky="ew", pady=10)
-        self.blue_btn = ct.CTkSlider(self.controls, from_=0, to=255, command=self.blue_func)
+        self.blue_btn = ct.CTkSlider(self.controls, from_=0, to=200, command=self.brightness_func)
         self.blue_btn.grid(row=9, column=0, sticky="ew", padx=10)
+
+        self.sharp_lbl = ct.CTkLabel(self.controls, text='Sharpness', text_color='#ffffff', bg_color='transparent')
+        self.sharp_lbl.grid(row=10, column=0, sticky="ew", pady=10)
+        self.sharp_btn = ct.CTkSlider(self.controls, from_=0, to=200, command=self.sharpness_func)
+        self.sharp_btn.grid(row=11, column=0, sticky="ew", padx=10)
 
         # save button
         self.save_btn = ct.CTkButton(self.controls, command=self.save_image_func, text='Save')
-        self.save_btn.grid(row=10, column=0, sticky="ews", padx=10, pady=10)
+        self.save_btn.grid(row=12, column=0, sticky="ews", padx=10, pady=10)
 
         # work panel
         self.field = ct.CTkFrame(self, fg_color='transparent', bg_color='transparent', width=700, corner_radius=10)
@@ -71,40 +76,57 @@ class App(ct.CTk):
             new_width = round(self.img.size[0] * width_percent)
             new_height = round(self.img.size[1] * width_percent)
             self.new_img = self.img.resize((new_width, new_height))
-            self.new_resize_img = self.new_img
-            self.editable_image = None
+            self.new_img = self.new_img.convert('RGB')
             self.add_photo.destroy()
             self.tatras = ImageTk.PhotoImage(self.new_img) # Вставляем изображение
             self.edit_image.place(anchor="c", relx=.5, rely=.5)
             self.edit_image.configure(image=self.tatras)
+            self.zoom_value, self.rotate_value, self.ench_value, self.contr_value, self.bright_value, self.sharp_value = 100, 0, 100, 100, 100, 100
         else:
             pass
 
     def zoom_func(self, value):
-        self.new_resize_img = self.new_img.resize((round(self.new_img.size[0] * (value / 100)), round(self.new_img.size[1] * (value / 100))))
-        self.tatras = ImageTk.PhotoImage(self.new_resize_img)
-        self.edit_image.configure(image=self.tatras)
-    
+        self.zoom_value = value
+        self.update_func(self.zoom_value, self.rotate_value, self.ench_value, self.contr_value, self.bright_value, self.sharp_value)
+
     def rotate_func(self, value):
-        self.editable_image = self.new_resize_img.rotate(value)
-        self.tatras = ImageTk.PhotoImage(self.editable_image)
+        self.rotate_value = value
+        self.update_func(self.zoom_value, self.rotate_value, self.ench_value, self.contr_value, self.bright_value, self.sharp_value)
+
+    def enchance_func(self, value):
+        self.ench_value = value
+        self.update_func(self.zoom_value, self.rotate_value, self.ench_value, self.contr_value, self.bright_value, self.sharp_value)
+
+    def contrast_func(self, value):
+        self.contr_value = value
+        self.update_func(self.zoom_value, self.rotate_value, self.ench_value, self.contr_value, self.bright_value, self.sharp_value)
+
+    def brightness_func(self, value):
+        self.bright_value = value
+        self.update_func(self.zoom_value, self.rotate_value, self.ench_value, self.contr_value, self.bright_value, self.sharp_value)
+
+    def sharpness_func(self, value):
+        self.sharp_value = value
+        self.update_func(self.zoom_value, self.rotate_value, self.ench_value, self.contr_value, self.bright_value, self.sharp_value)
+
+    def update_func(self, zoom_val, rotate_val, ench_val, contr_val, bright_val, sharp_val):
+        new_resize_img = self.new_img.resize((round(self.new_img.size[0] * (zoom_val / 100)), round(self.new_img.size[1] * (zoom_val / 100))))
+        editable_image = new_resize_img.rotate(rotate_val)
+        saturation = ImageEnhance.Color(editable_image).enhance(ench_val / 100)
+        contrast = ImageEnhance.Contrast(saturation).enhance(contr_val / 100)
+        brightness = ImageEnhance.Brightness(contrast).enhance(bright_val / 100)
+        sharpness = ImageEnhance.Sharpness(brightness).enhance(sharp_val / 100)
+
+        self.end_img = sharpness
+        self.tatras = ImageTk.PhotoImage(self.end_img)
         self.edit_image.configure(image=self.tatras)
-
-    def red_func(self, value):
-        pass
-
-    def green_func(self, value):
-        pass
-
-    def blue_func(self, value):
-        pass
 
     def save_image_func(self):
-        if(self.editable_image):
-            x, y = self.editable_image.size[0], self.editable_image.size[1]
-            self.editable_image.resize((self.img.size[0], self.img.size[1]))
-            self.editable_image.save('some.png', 'png')
-            self.editable_image.resize((x, y))
+        if(self.end_img):
+            x, y = self.end_img.size[0], self.end_img.size[1]
+            self.end_img.resize((self.img.size[0], self.img.size[1]))
+            self.end_img.save('some.png', 'png', quality=100)
+            self.end_img.resize((x, y))
 
     def __del__(self):
         print('destroed!')
